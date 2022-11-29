81b1acf4b1cf1ab378ba4dff864c35395cc7ae11
NivetikaRajasekar <101450267+NivetikaRajasekar@users.noreply.github.com>
2022-11-29 21:58:13 +0530

    Delete UserAvatar.js

--- UserAvatar.js
+++
@@ -1,9 +0,0 @@
-const UserAvatar = ({ User }) => {
-  return (
-    <div className="UserAvatar">
-      <img src={User.Avatar} alt={User.FullName} width={64} />
-    </div>
-  );
-};
-
-export default UserAvatar;
